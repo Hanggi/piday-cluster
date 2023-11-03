22d12436f325915b3749395afc803bd8fd05e338
Hanggi <>
2023-11-03 17:26:06 +0900

    Add vscode locales paths

--- piday-web/src/app/[locale]/_components/layout/Header.tsx
+++ piday-web/src/app/[locale]/_components/layout/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 w-full bg-yellow-500 h-16">
-      <nav className="container mx-auto h-full flex justify-between items-center">
+      <nav className="container mx-auto px-2 h-full flex justify-between items-center">
         <Link href="/">
           <div className="relative h-12 w-28">
             <Image
