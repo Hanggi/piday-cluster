e07102e76ccc583cfd7b3346b571e07d5123fb2e
sohanemon <m.sohanemon@gmail.com>
2023-11-11 23:05:51 +0600

    Add invitation, commission, transaction data to ranking data

--- piday-web/src/app/[locale]/ranking/_components/rankData.ts
+++ piday-web/src/app/[locale]/ranking/_components/rankData.ts
@@ -42,7 +42,40 @@ const personal = [
     totalPoints: 2344,
   },
 ] as const;
+const invitation = [
+  {
+    username: "张**",
+    numberOfInvitedUserRegistrations: 2400,
+    totalPoints: 2344,
+  },
+  {
+    username: "张**",
+    numberOfInvitedUserRegistrations: 2400,
+    totalPoints: 2344,
+  },
+] as const;
+const commission = [
+  {
+    username: "张**",
+    numberOfLandHoldings: 2400,
+    totalPoints: 2344,
+  },
+] as const;
+const transaction = [
+  {
+    nation: "中国",
+    numberOfLandTransactions: 2400,
+    numberOfLandTransactions2: 1000,
+    turnover: 2000,
+  },
+] as const;
 
-export const rankData = { country, personal };
+export const rankData = {
+  country,
+  personal,
+  invitation,
+  commission,
+  transaction,
+};
 
 export type RankDataKey = keyof typeof rankData;
