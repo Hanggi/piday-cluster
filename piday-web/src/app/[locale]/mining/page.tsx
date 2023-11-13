4c897b29cdbed2ead7b58754f7182cd8418e185b
sohanemon <m.sohanemon@gmail.com>
2023-11-13 20:42:03 +0600

    feat(mining): add mining page with banner image and reward section feat(Wrapper.tsx): add optional 'modal' prop to Wrapper component

--- piday-web/src/app/[locale]/mining/page.tsx
+++ piday-web/src/app/[locale]/mining/page.tsx
@@ -1,17 +1,31 @@
 import { Wrapper } from "@/src/components/Wrapper";
 
+import { Button } from "@mui/joy";
+
+import Image from "next/image";
+
 export const metadata = {
   title: "Mining",
 };
 
 export default function MiningPage() {
   return (
-    <section>
-      <Wrapper>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum aperiam
-        rem odio cumque, est nam porro, nostrum consequuntur quidem delectus eum
-        fugiat ipsam blanditiis, dignissimos quis cupiditate ratione
-        voluptatibus aliquam.
+    <section className="container">
+      <Wrapper className="flex flex-wrap lg:h-96 gap-14 max-md:flex-col ">
+        <Image
+          alt="banner"
+          className="flex-1"
+          height={244}
+          src={`/img/mining-banner.png`}
+          width={770}
+        />
+        <aside className="min-w-[300px] aspect-square flex flex-col p-5 h-full bg-neutral-100 rounded-2xl">
+          <div className="flex-1 flex flex-col items-center justify-center gap-3">
+            <h4 className="text-secondary text-6xl font-bold">200</h4>
+            <p className="text-black/40">待领取积分</p>
+          </div>
+          <Button className="w-full">领取积分</Button>
+        </aside>
       </Wrapper>
     </section>
   );
--- piday-web/src/components/Wrapper.tsx
+++ piday-web/src/components/Wrapper.tsx
@@ -2,13 +2,16 @@ import { ComponentProps } from "react";
 
 import { cn } from "../utils/cn";
 
-type WrapperProps = ComponentProps<"section">;
+type WrapperProps = ComponentProps<"section"> & {
+  modal?: boolean;
+};
 
-export function Wrapper({ className, ...props }: WrapperProps) {
+export function Wrapper({ className, modal, ...props }: WrapperProps) {
   return (
     <section
       className={cn(
         "md:p-7 p-4 bg-white shadow rounded-lg  md:rounded-xl",
+        { "w-full md:w-[450px]": modal },
         className,
       )}
       {...props}
