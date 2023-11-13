0ffe7fc7da76c0e6aa9f955556f4a4dbf7999f4e
sohanemon <m.sohanemon@gmail.com>
2023-11-13 12:37:07 +0600

    chore(Footer.tsx): add social media icons and links to the footer fix(Footer.tsx): fix typo in social media links

--- piday-web/src/app/[locale]/_components/layout/Footer.tsx
+++ piday-web/src/app/[locale]/_components/layout/Footer.tsx
@@ -1,11 +1,46 @@
 "use client";
 
+import { cn } from "@/src/utils/cn";
+
+import Link from "next/link";
+
 import Navbar, { navType } from "./Navbar";
 
 export default function Footer() {
   return (
     <section className="w-full  bg-primary">
-      <Navbar navType={navType.footer}>hello</Navbar>
+      <Navbar navType={navType.footer}>
+        <div className="flex items-center gap-5">
+          {socials.map((social) => (
+            <Link href={social.href} key={social.href} target="_blank">
+              <i className={cn("text-lg", social.icon)} />
+            </Link>
+          ))}
+        </div>
+      </Navbar>
+      <hr className="opacity-50" />
+      <center className="text-black/80 py-4 text-sm font-normal">
+        Copyright © Piday 2023.
+      </center>
     </section>
   );
 }
+
+const socials = [
+  {
+    icon: "ri-facebook-circle-fill",
+    href: "https:facebook.com/",
+  },
+  {
+    icon: "ri-twitter-x-line",
+    href: "https:x.com/",
+  },
+  {
+    icon: "ri-linkedin-fill",
+    href: "https:linkedin.com/in/",
+  },
+  {
+    icon: "ri-instagram-line",
+    href: "https:instagram.com/",
+  },
+];
