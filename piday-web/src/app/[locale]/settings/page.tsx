d2496afb43c5d5aa4a8a5d2a4db48bedb6fb3c55
sohanemon <m.sohanemon@gmail.com>
2023-11-17 21:49:41 +0600

    feat: Implement user authentication

---
+++ piday-web/src/app/[locale]/settings/page.tsx
@@ -0,0 +1,21 @@
+import { BreadCrumb } from "@/src/components/BreadCrumb";
+import { WrapperCard } from "@/src/components/WrapperCard";
+
+import { Form } from "./_components/Form";
+import { User } from "./_components/User";
+
+export const metadata = {
+  title: "Settings",
+};
+
+export default function SettingPage() {
+  return (
+    <main className="container">
+      <BreadCrumb />
+      <WrapperCard className="p-0 md:p-0">
+        <User />
+        <Form />
+      </WrapperCard>
+    </main>
+  );
+}
