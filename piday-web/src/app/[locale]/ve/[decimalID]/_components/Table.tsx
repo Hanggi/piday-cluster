2e0080bf974b476068eefde9d4b8d3fe75ade4e3
sohanemon <m.sohanemon@gmail.com>
2023-12-07 23:29:41 +0600

    feat(page): Add table component
    The commit adds a new table component to the page, enhancing the content
    display and user experience.

---
+++ piday-web/src/app/[locale]/ve/[decimalID]/_components/Table.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  Table as TableRoot,
+  TableRow,
+} from "@/src/components/Table";
+import { WrapperCard } from "@/src/components/WrapperCard";
+import { cn } from "@/src/utils/cn";
+
+import Image from "next/image";
+
+import { ComponentProps } from "react";
+import { useTranslation } from "react-i18next";
+
+type TableProps = ComponentProps<typeof WrapperCard>;
+
+export function Table({ className, ...props }: TableProps) {
+  const { t } = useTranslation("map");
+  return (
+    <WrapperCard className={cn("w-full mb-6 container", className)} {...props}>
+      <h4 className="text-black text-xl font-semibold font-['PingFang SC']">
+        {t("landHistory")}
+      </h4>
+      <TableRoot className="mt-5">
+        <TableHeader>
+          <TableRow>
+            {[, "type", "price", "date", "user"].map((head) => (
+              <TableHead key={head}>{t(`column.${head}`)}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Array.from(Array(10).keys()).map((idx) => (
+            <TableRow key={idx}>
+              {data.map((el, idx) => (
+                <TableCell key={el}>
+                  {idx === 1 && (
+                    <Image
+                      src={"/img/icons/pid.png"}
+                      alt="pid"
+                      width={24}
+                      height={24}
+                      className="aspect-square inline-block object-contain w-5 mr-1"
+                    />
+                  )}
+                  {el}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </TableRoot>
+    </WrapperCard>
+  );
+}
+
+const data = ["拍卖开始", "150", "2023-08-25 16:00:21", "0x4bb...0707"];
--- piday-web/src/app/[locale]/ve/[decimalID]/page.tsx
+++ piday-web/src/app/[locale]/ve/[decimalID]/page.tsx
@@ -1,6 +1,7 @@
 import { decimalToHexID } from "@/src/components/virtual-estate-map/h3";
 
 import VirtualEstateMapClientWrapper from "../../_components/home-ve-map/VirtualEstateMapClientWrapper";
+import { Table } from "./_components/Table";
 import VirtualEstateDetailCard from "./_components/VirtualEstateDetailCard";
 
 const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
@@ -15,14 +16,18 @@ export default function VirtualEstateDetailPage({
   const hexID = decimalToHexID(decimalID);
 
   return (
-    <div className="container mx-auto h-screen grid grid-cols-1 md:grid-cols-2 gap-5 py-4 ">
-      <div className="w-full max-h-[500px] relative rounded-lg">
-        <VirtualEstateMapClientWrapper
-          defaultHexID={hexID}
-          token={MAPBOX_ACCESS_TOKEN as string}
-        />
+    <main>
+      <div className="container mx-auto h-screen grid grid-cols-1 md:grid-cols-2 gap-5 py-4 ">
+        <div className="w-full max-h-[500px] relative rounded-lg">
+          <VirtualEstateMapClientWrapper
+            defaultHexID={hexID}
+            token={MAPBOX_ACCESS_TOKEN as string}
+          />
+        </div>
+        <VirtualEstateDetailCard hexID={hexID} />
       </div>
-      <VirtualEstateDetailCard hexID={hexID} />
-    </div>
+      <Table />
+      <br />
+    </main>
   );
 }
