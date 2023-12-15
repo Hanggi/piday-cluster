406db4d2c20195dece19000253a7537c1b64f11c
zawar hassan <zawarhassan112@gmail.com>
2023-12-15 18:17:30 +0500

    Added endpoint to get all virtual estates for current signed in user

--- piday-server/src/virtual-estate/virtual-estate.controller.ts
+++ piday-server/src/virtual-estate/virtual-estate.controller.ts
@@ -9,15 +9,51 @@ import {
   Param,
   Req,
   Res,
+  UseGuards,
 } from "@nestjs/common";
 
 import { AuthenticatedRequest } from "../lib/keycloak/interfaces/authenticated-request";
+import { KeycloakJwtGuard } from "../lib/keycloak/keycloak-jwt.guard";
 import { VirtualEstateResponseDto } from "./dto/virtual-estate.dto";
 import { VirtualEstateService } from "./virtual-estate.service";
 
 @Controller("virtual-estate")
 export class VirtualEstateController {
   constructor(private readonly virtualEstateService: VirtualEstateService) {}
+
+  @Get("all-by-user")
+  @UseGuards(KeycloakJwtGuard)
+  async getAllVirtualEstates(
+    @Req() req: AuthenticatedRequest,
+    @Res() res: Response,
+  ) {
+    const virtualEstates =
+      await this.virtualEstateService.getAllVirtualEstatesForSignedUser(
+        req.user.userID,
+      );
+
+    if (!virtualEstates) {
+      res.status(HttpStatus.NOT_FOUND).json({
+        success: false,
+        data: null,
+        message: "No virtual estates found by this user",
+      });
+    }
+
+    res.status(HttpStatus.OK).json({
+      data: virtualEstates,
+      success: true,
+      message: "Virtual states found successfully",
+    });
+    try {
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        "Internal Server Error",
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
   @Get(":hexID")
   async getHexID(
--- piday-server/src/virtual-estate/virtual-estate.service.ts
+++ piday-server/src/virtual-estate/virtual-estate.service.ts
@@ -22,4 +22,20 @@ export class VirtualEstateService {
 
     return virtualEstate;
   }
+
+  async getAllVirtualEstatesForSignedUser(
+    userId: string,
+  ): Promise<VirtualEstate[]> {
+    console.log("Got The Request in the service", userId);
+    const virtualEstates = await this.prisma.virtualEstate.findMany({
+      where: {
+        ownerID: userId,
+      },
+      include: {
+        owner: true,
+      },
+    });
+    console.log(virtualEstates);
+    return virtualEstates;
+  }
 }
