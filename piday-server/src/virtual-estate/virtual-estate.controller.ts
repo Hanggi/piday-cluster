e9f11d2945a2d708ff0b6044d0159439edc47ec1
zawar hassan <zawarhassan112@gmail.com>
2023-12-21 17:31:40 +0500

    removed keycloak made listing endpoint public

--- piday-server/src/virtual-estate/virtual-estate.controller.ts
+++ piday-server/src/virtual-estate/virtual-estate.controller.ts
@@ -103,7 +103,7 @@ export class VirtualEstateController {
       );
     }
   }
-  @UseGuards(KeycloakJwtGuard)
+  
   @Get(":hexID/listing")
   async getVirtualEstateListingOffers(
     @Param("hexID") hexID,
