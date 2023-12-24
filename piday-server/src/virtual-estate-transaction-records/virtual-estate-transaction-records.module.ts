0993e8e2025dd297a3372cb4dd3458912364bad1
zawar hassan <zawarhassan112@gmail.com>
2023-12-24 05:41:21 +0500

    Fix dependancy issue

--- piday-server/src/virtual-estate-transaction-records/virtual-estate-transaction-records.module.ts
+++ piday-server/src/virtual-estate-transaction-records/virtual-estate-transaction-records.module.ts
@@ -3,9 +3,10 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "../lib/prisma/prisma.service";
 import { VirtualEstateTransactionRecordsController } from "./virtual-estate-transaction-records.controller";
 import { VirtualEstateTransactionRecordsService } from "./virtual-estate-transaction-records.service";
+import { VirtualEstateListingService } from "../virtual-estate-listing/virtual-estate-listing.service";
 
 @Module({
   controllers: [VirtualEstateTransactionRecordsController],
-  providers: [VirtualEstateTransactionRecordsService, PrismaService],
+  providers: [VirtualEstateTransactionRecordsService, PrismaService, VirtualEstateListingService],
 })
 export class VirtualEstateTransactionRecordsModule {}
