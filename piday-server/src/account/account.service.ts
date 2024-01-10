50bbe6d1daa99254c553dd97e0281d219b249644
Hanggi <>
2024-01-10 15:00:06 +0900

    test version for logging

--- piday-server/src/account/account.service.ts
+++ piday-server/src/account/account.service.ts
@@ -5,13 +5,13 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../lib/prisma/prisma.service";
 import { ZERO_DECIMAL } from "../lib/prisma/utils/zerro-decimal";
 import { RechargeRecordResponseDto } from "./dto/rechargeRecords.dto";
-import { RechargeRecords } from "@prisma/client";
 
 @Injectable()
 export class AccountService {
   constructor(private prisma: PrismaService) {}
 
   async getMyBalance({ userID }: { userID: string }): Promise<Decimal> {
+    console.log("userID", userID);
     const totalBalance = await this.prisma.rechargeRecords.aggregate({
       _sum: {
         amount: true,
@@ -20,6 +20,7 @@ export class AccountService {
         ownerID: userID,
       },
     });
+    console.log("totalBalance", totalBalance);
 
     const balance = totalBalance._sum.amount || ZERO_DECIMAL;
 
