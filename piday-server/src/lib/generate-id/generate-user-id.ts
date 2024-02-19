7c2b7671f673115345ef3ac28f76c9e799be43f6
zawar hassan <zawarhassan112@gmail.com>
2024-02-19 19:07:09 +0500

    fixed invite code logic to a simpler form

--- piday-server/src/lib/generate-id/generate-user-id.ts
+++ piday-server/src/lib/generate-id/generate-user-id.ts
@@ -1,43 +1,25 @@
-import base64url from "base64url";
-import crypto from "crypto";
 import Hashids from "hashids";
 
 const secretKey = process.env.HASH_ID_SECRET_KEY;
 
 const hashIDForUser = new Hashids(secretKey, 8, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
+const hashIDforInviteCode = new Hashids(
+  secretKey,
+  6,
+  "abcdefghijklmnopqrstuvwxyz",
+);
 export const generateUserVisibleId = (userId) => {
   const encrypted = hashIDForUser.encode(userId);
   return encrypted;
 };
 
 export const generateInvitationCode = (userId: number) => {
-  const initializationVector = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(
-    "aes-256-cbc",
-    secretKey,
-    initializationVector,
-  );
-  let inviteCode = cipher.update(JSON.stringify(userId), "utf-8", "hex");
-  inviteCode += cipher.final("hex");
-  const result = {
-    initializationVector: base64url.encode(initializationVector),
-    inviteCode,
-  };
-
-  return result;
+  const encrypted = hashIDforInviteCode.encode(userId);
+  return encrypted;
 };
 
-export const decodeInviteCode = (
-  inviteCode: string,
-  initializationVector: string,
-) => {
-  const decipher = crypto.createDecipheriv(
-    "aes-256-cbc",
-    secretKey,
-    base64url.toBuffer(initializationVector),
-  );
-  let decryptedId = decipher.update(inviteCode, "hex", "utf-8");
-  decryptedId += decipher.final("utf-8");
-  return decryptedId;
+export const decodeInviteCode = (inviteCode: string) => {
+  const decodedID = hashIDforInviteCode.decode(inviteCode);
+  return decodedID[0] as number
 };
