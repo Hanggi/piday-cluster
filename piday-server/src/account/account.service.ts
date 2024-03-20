f87d769c53c226aba67c2228cd81bdfcfd89a0bf
zawar hassan <zawarhassan112@gmail.com>
2024-03-20 23:11:01 +0400

    Transfer amount to other user

--- piday-server/src/account/account.service.ts
+++ piday-server/src/account/account.service.ts
@@ -98,6 +98,9 @@ export class AccountService {
             {
               email: to,
             },
+            {
+              keycloakID: to,
+            },
           ],
         },
       });
