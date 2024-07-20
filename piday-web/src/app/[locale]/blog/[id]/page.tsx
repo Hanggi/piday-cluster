0e5c7b02f6ad355d6a9c8fa67c90edb77230a388
zawar hassan <zawarhassan112@gmail.com>
2024-07-20 16:05:44 -0500

    Added blogs

--- piday-web/src/app/[locale]/blog/[id]/page.tsx
+++ piday-web/src/app/[locale]/blog/[id]/page.tsx
@@ -1,26 +1,70 @@
+import fs from "fs";
+import path from "path";
+
+import Card from "@mui/joy/Card";
+
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
-type Blog = {
-  id: number;
-  title: string;
-  content: string;
+import React from "react";
+import ReactMarkdown from "react-markdown";
+
+type BlogProps = {
+  params: {
+    id: string;
+  };
 };
 
-const BlogPost = async ({ params }: { params: { id: string } }) => {
-  const res = await fetch(`${process.env.NEXTAUTH_URL}/blogs.json`);
-  const blogs: Blog[] = await res.json();
-  const blog = blogs.find((blog) => blog.id.toString() === params.id);
+async function getBlogContent(id: string): Promise<string | null> {
+  const blogDir = path.join(process.cwd(), "public", "blogs");
+  const filePath = path.join(blogDir, `${id}.md`);
 
-  if (!blog) {
+  if (fs.existsSync(filePath)) {
+    return fs.readFileSync(filePath, "utf8");
+  } else {
+    return null;
+  }
+}
+
+export default async function BlogPost({ params }: BlogProps) {
+  const { id } = params;
+  const content = await getBlogContent(id);
+
+  if (!content) {
     notFound();
   }
 
   return (
-    <div>
-      <h1>{blog.title}</h1>
-      <p>{blog.content}</p>
-    </div>
-  );
-};
+    <Card className="mt-8" size="lg">
+      <div className="min-h-screen py-10">
+        <div className="container mx-auto px-6 md:px-12 lg:px-24">
+          <div className="text-center mb-10">
+            <h1 className="text-5xl font-extrabold text-[rgba(89,59,139,100)] mb-4">
+              {id}
+            </h1>
+          </div>
 
-export default BlogPost;
+          <article className="prose prose-lg">
+            <ReactMarkdown
+              components={{
+                img: ({ src, alt }) => (
+                  <div className="flex justify-center mb-10">
+                    <Image
+                      src={src as string}
+                      alt={alt || ""}
+                      width={800}
+                      height={400}
+                      className="rounded-lg shadow-lg"
+                    />
+                  </div>
+                ),
+              }}
+            >
+              {content}
+            </ReactMarkdown>
+          </article>
+        </div>
+      </div>
+    </Card>
+  );
+}
--- piday-web/src/app/[locale]/blog/page.tsx
+++ piday-web/src/app/[locale]/blog/page.tsx
@@ -1,29 +1,46 @@
+import fs from "fs";
+import path from "path";
+
 import Link from "next/link";
 
 type Blog = {
-  id: number;
+  id: string;
   title: string;
-  content: string;
 };
 
-const Home = async () => {
-  const res = await fetch(`${process.env.NEXTAUTH_URL}/blogs.json`);
-  const blogs: Blog[] = await res.json();
+async function getBlogs(): Promise<Blog[]> {
+  const blogDir = path.join(process.cwd(), "public", "blogs");
+  const filenamesAll = fs.readdirSync(blogDir);
+  const filenames = filenamesAll.filter((filename) => filename.includes(".md"));
+
+  return filenames.map((filename) => {
+    console.log("Reading files ", filename);
+
+    const filePath = path.join(blogDir, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const title = fileContents.split("\n")[0].replace("# ", "");
+    return {
+      id: filename.replace(".md", ""),
+      title,
+    };
+  });
+}
+
+export default async function Home() {
+  const blogs = await getBlogs();
 
   return (
-    <div>
-      <h1>Blog List</h1>
-      <ul>
+    <div className="max-w-2xl mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center mb-8">Blog List</h1>
+      <ul className="list-disc list-inside">
         {blogs.map((blog) => (
-          <li key={blog.id}>
+          <li key={blog.id} className="mb-4">
             <Link href={`/blog/${blog.id}`}>
-              <h2>{blog.title}</h2>
+              <h2 className="text-blue-500 hover:underline">{blog.title}</h2>
             </Link>
           </li>
         ))}
       </ul>
     </div>
   );
-};
-
-export default Home;
+}
