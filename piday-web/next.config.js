96525794ab9383be40a30764ad2c29d10c9e797f
Hanggi <>
2024-09-22 22:26:47 +0900

    Try add PWA next

--- piday-web/next.config.js
+++ piday-web/next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   output: "standalone",
+// };
+
+const withPWA = require("next-pwa")({
+  disable: process.env.NODE_ENV === "development",
+  dest: "public",
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
-};
-
-module.exports = nextConfig;
+});
