92f01ea9c2f6055e1345ab2670fb474631bd19fb
Hanggi <>
2024-10-16 01:03:31 +0900

    Use MapboxOverlay to refactor the map
    Add geocoder to map

--- piday-web/src/components/virtual-estate-map/VirtualEstateMap.tsx
+++ piday-web/src/components/virtual-estate-map/VirtualEstateMap.tsx
@@ -2,14 +2,12 @@
 
 import { useLazyGetClusterInAreaQuery } from "@/src/features/virtual-estate/api/virtualEstateAPI";
 import { H3ClusterItem } from "@/src/features/virtual-estate/interface/virtual-estate.interface";
-import { FlyToInterpolator, MapViewState } from "@deck.gl/core";
-import { PickingInfo } from "@deck.gl/core";
-import type { ViewStateChangeParameters } from "@deck.gl/core";
+import { FlyToInterpolator } from "@deck.gl/core";
 import { H3ClusterLayer, H3HexagonLayer } from "@deck.gl/geo-layers";
 import { TextLayer } from "@deck.gl/layers";
-import DeckGL from "@deck.gl/react";
-import { ZoomWidget } from "@deck.gl/widgets";
+import { MapboxOverlay } from "@deck.gl/mapbox";
 import "@deck.gl/widgets/stylesheet.css";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import { geoToH3, h3ToGeo, kRing } from "h3-js";
 import { debounce } from "lodash";
 
@@ -17,12 +15,14 @@ import Card from "@mui/joy/Card";
 import Typography from "@mui/joy/Typography";
 
 import { useCallback, useEffect, useRef, useState } from "react";
-import type { MapRef } from "react-map-gl";
-import Map, { GeolocateControl, NavigationControl, useMap } from "react-map-gl";
+import type { MapLayerMouseEvent, MapRef } from "react-map-gl";
+import Map, { GeolocateControl, NavigationControl } from "react-map-gl";
+
+import GeocoderControl from "./GeocoderControl";
 
 const SHOW_HEXAGON_LAYER_FROM_ZOOM = 15;
 
-const INITIAL_VIEW_STATE: MapViewState = {
+const INITIAL_VIEW_STATE = {
   longitude: -122.41669,
   latitude: 37.7853,
   zoom: 3,
@@ -59,45 +59,34 @@ export default function VirtualEstateMap({
 }: Props) {
   const mapRef = useRef<MapRef | null>(null);
 
-  const [viewState, setViewState] = useState<MapViewState>(INITIAL_VIEW_STATE);
+  const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
   const [hexagons, setHexagons] = useState<VirtualEstate[]>([]);
-  // hexagonsCenter is the hexagon that is currently in the center of the hexagons array
-  const [hexagonsCenter, setHexagonCenter] = useState<string>(""); // hex ID
-  // centerHex is the hexagon that is currently in the center of the map
-  const [centerHex, setCenterHex] = useState<string>(""); // hex ID
-  // mounted is used to prevent the map from rendering before the hexagons are ready
+  const [hexagonsCenter, setHexagonCenter] = useState<string>("");
+  const [centerHex, setCenterHex] = useState<string>("");
   const [mounted, setMounted] = useState<boolean>(false);
-  const [layers, setLayers] = useState<any[]>([]); // [H3HexagonLayer
-
   const [selectedHexID, setSelectedHexID] = useState<string>("");
-
-  const h3Index = geoToH3(
-    viewState.latitude,
-    viewState.longitude,
-    viewState.zoom,
-  );
 
   const [clusterData, setClusterData] = useState<H3ClusterItem[]>([]);
   const [clusterMaxCount, setClusterMaxCount] = useState<number>(255);
   const [clusterDepth, setClusterDepth] = useState<number | null>(null);
   const [triggerGetCluster, clusterResult] = useLazyGetClusterInAreaQuery();
 
-  useEffect(() => {
-    const ch = centerHex; // 示例中心六边形
+  // 初始化 MapboxOverlay
+  const overlay = useRef<MapboxOverlay | null>(null);
 
+  useEffect(() => {
+    const ch = centerHex;
     if (viewState.zoom >= SHOW_HEXAGON_LAYER_FROM_ZOOM) {
       if (ch == hexagonsCenter && hexagons.length > 0) {
         return;
       }
-      const hexagonsMatirx = kRing(ch, 60).map((hex) => {
-        return {
-          hexID: hex,
-        };
-      });
+      const hexagonsMatrix = kRing(ch, 60).map((hex) => ({
+        hexID: hex,
+      }));
       setHexagonCenter(ch);
-      setHexagons(hexagonsMatirx as any);
+      setHexagons(hexagonsMatrix);
     } else {
-      if (hexagons.length != 0) {
+      if (hexagons.length !== 0) {
         setHexagons([]);
       }
     }
@@ -105,10 +94,9 @@ export default function VirtualEstateMap({
 
   useEffect(() => {
     let depth = 2;
-    let hexID = "8212d7fffffffff";
+    let hexID = geoToH3(viewState.latitude, viewState.longitude, 2);
     if (viewState.zoom > 0 && viewState.zoom <= 4) {
       depth = 2;
-      hexID = geoToH3(viewState.latitude, viewState.longitude, 2);
       setClusterDepth(2);
     } else if (viewState.zoom > 4 && viewState.zoom <= 6) {
       depth = 4;
@@ -134,7 +122,7 @@ export default function VirtualEstateMap({
       setClusterDepth(null);
     }
 
-    if (viewState.zoom < 14 && clusterDepth != depth) {
+    if (viewState.zoom < 14 && clusterDepth !== depth) {
       triggerGetCluster({
         hexID: hexID,
         depth,
@@ -149,83 +137,69 @@ export default function VirtualEstateMap({
 
   useEffect(() => {
     if (clusterResult.data) {
-      // get max count from cluster
       const maxCount = Math.max(
         ...clusterResult.data.cluster.map((item) => item.count),
       );
       setClusterMaxCount(maxCount);
-
       setClusterData(clusterResult.data.cluster);
     }
   }, [clusterResult.data]);
 
   useEffect(() => {
-    setLayers([
-      hexagons &&
+    const layers = [
+      hexagons.length > 0 &&
         new H3HexagonLayer({
           id: "h3-hexagon-layer",
           data: hexagons,
           pickable: true,
           wireframe: true,
-          extruded: false, // 确保六边形不是挤出的
-          filled: true, // 使六边形为空心
+          extruded: false,
+          filled: true,
           elevationScale: 1,
-          getLineWidth: () => {
-            return 0.1;
-          },
-          getText: () => {
-            return "hihi";
-          },
+          getLineWidth: () => 0.1,
+          getText: () => "hihi",
           getLineColor: (d) => {
-            if (selectedHexID == d.hexID) {
-              // returen yellow selected color
+            if (selectedHexID === d.hexID) {
               return [255, 255, 0, 200];
             }
-
-            // If hex ID is in onSale, return green
             if (onSaleList?.includes(d.hexID)) {
               return [0, 255, 0, 200];
             }
-
             if (soldList?.includes(d.hexID)) {
               return [255, 0, 0, 200];
             }
-
             return [112, 48, 160];
-          }, // 设置六边形的边线颜色
-          getHexagon: (d) => d.hexID, // 从数据对象中获取H3索引
+          },
+          getHexagon: (d) => d.hexID,
           getFillColor: (d) => {
-            if (selectedHexID == d.hexID) {
+            if (selectedHexID === d.hexID) {
               return [112, 48, 160, 200];
             }
-
-            // If hex ID is in onSale, return green
             if (onSaleList?.includes(d.hexID)) {
               return [0, 255, 0, 200];
             }
-
             if (soldList?.includes(d.hexID)) {
               return [112, 48, 160, 100];
             }
-
             return [255, 255, 255, 0];
           },
-          // getElevation: (d) => 10, // 设置六边形的高度，仅当extruded为true时有效
           updateTriggers: {
             getFillColor: [selectedHexID, onSaleList, soldList],
           },
         }),
-      clusterData &&
+      clusterData.length > 0 &&
         clusterDepth &&
         new H3ClusterLayer<H3ClusterItem>({
           id: "H3ClusterLayer",
           data: clusterData,
-
           stroked: true,
           getHexagons: (d: H3ClusterItem) => d.hexIds,
-          getFillColor: (d: H3ClusterItem) => {
-            return [112, 48, 160, 70 + (d.count / clusterMaxCount) * 160];
-          },
+          getFillColor: (d: H3ClusterItem) => [
+            112,
+            48,
+            160,
+            70 + (d.count / clusterMaxCount) * 160,
+          ],
           getLineColor: [255, 255, 255, 100],
           lineWidthMinPixels: 1,
           pickable: true,
@@ -235,61 +209,61 @@ export default function VirtualEstateMap({
         data: hexagons,
         getPosition: (d) => d.coordinates,
         getText: (d) => {
-          return "FFFFF";
           if (soldList?.includes(d.hexID)) {
             return "SOLD";
           }
-
           return "VV";
         },
-        // getColor: [255, 255, 255, 1000], // 文本颜色
         getSize: 32,
-        getAngle: 0,
         getTextAnchor: "middle",
         getAlignmentBaseline: "center",
         updateTriggers: {
           getText: [soldList],
         },
       }),
-    ]);
+    ];
 
-    setViewState((vs) => {
-      return {
-        ...vs,
-      };
-    });
+    // 初始化 MapboxOverlay 并传入 layers
+    if (!overlay.current) {
+      overlay.current = new MapboxOverlay({ layers: layers } as any);
+    } else {
+      overlay.current.setProps({ layers: layers } as any);
+    }
   }, [hexagons, onSaleList, selectedHexID, soldList, clusterData]);
 
-  // When the default hex id is non-empty, initialize coordinate animation
   useEffect(() => {
     if (defaultHexID) {
       const target = h3ToGeo(defaultHexID);
-
       setSelectedHexID(defaultHexID);
       setCenterHex(defaultHexID);
-      setTimeout(() => {
-        setViewState((vs) => ({
-          ...vs,
-          longitude: target[1],
-          latitude: target[0],
-          zoom: 17,
 
-          transitionDuration: withoutAnimation ? 0 : 3000, // 设置较长的过渡时间，例如 3 秒
-          transitionInterpolator: withoutAnimation
-            ? undefined
-            : new FlyToInterpolator(),
-        }));
+      setTimeout(() => {
+        console.log(mapRef);
+
+        if (withoutAnimation) {
+          mapRef.current?.jumpTo({
+            center: [target[1], target[0]], // 经度和纬度
+            zoom: 17, // 目标缩放级别
+          });
+        } else {
+          mapRef.current?.flyTo({
+            center: [target[1], target[0]], // 经度和纬度
+            zoom: 17, // 目标缩放级别
+            speed: 2.5, // 速度（越大越快）
+            curve: 1, // 动画曲线的弯曲程度（越大越平滑）
+            easing: (t) => t, // 自定义动画缓动函数
+            essential: true, // 如果是 false，则地图在用户交互期间会中断飞行动画
+          });
+        }
       }, 200);
     }
   }, [defaultHexID, withoutAnimation]);
 
-  // Debounce to set center hexagon
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const debounceToSetCenterHex = useCallback(
     debounce((vs) => {
       if (vs.zoom >= SHOW_HEXAGON_LAYER_FROM_ZOOM) {
         const hexID = geoToH3(vs.latitude, vs.longitude, 12);
-        if (hexID != centerHex) {
+        if (hexID !== centerHex) {
           setCenterHex(hexID);
           onCenterHexChange && onCenterHexChange(hexID);
         }
@@ -299,25 +273,27 @@ export default function VirtualEstateMap({
   );
 
   const handleMapViewChange = useCallback(
-    (params: ViewStateChangeParameters) => {
+    (params: any) => {
+      // console.log(params);
+      // console.log(mounted);
       if (params.viewState.zoom >= SHOW_HEXAGON_LAYER_FROM_ZOOM) {
         debounceToSetCenterHex(params.viewState);
       }
-      if (mounted) {
-        setViewState(params.viewState as MapViewState);
-      }
+      setViewState(params.viewState);
+      // if (mounted) {
+      // }
     },
     [debounceToSetCenterHex, mounted],
   );
 
   const handleClickHexagon = useCallback(
-    (pi: PickingInfo, e: any) => {
-      if (e.target.className == "deck-widget-icon") {
-        return;
-      }
-
-      if (pi.coordinate) {
-        const hexID = geoToH3(pi.coordinate[1], pi.coordinate[0], 12);
+    (e: MapLayerMouseEvent) => {
+      // console.log(e);
+      // if (e.target.className === "deck-widget-icon") {
+      //   return;
+      // }
+      if (e.lngLat) {
+        const hexID = geoToH3(e.lngLat.lat, e.lngLat.lng, 12);
         setSelectedHexID(hexID);
         setViewState({
           ...viewState,
@@ -331,66 +307,34 @@ export default function VirtualEstateMap({
 
   return (
     <div className="relative w-full h-full">
-      <DeckGL
-        controller={true}
+      <Map
+        mapStyle="mapbox://styles/mapbox/streets-v12"
+        mapboxAccessToken={token}
         initialViewState={viewState}
-        layers={layers}
-        onAfterRender={() => {
-          setTimeout(() => {
-            setMounted(true);
-          }, 500);
+        onLoad={() => {
+          if (overlay.current) {
+            mapRef.current?.getMap().addControl(overlay.current as any);
+          }
         }}
         onClick={handleClickHexagon}
-        onViewStateChange={handleMapViewChange}
-        widgets={[
-          new ZoomWidget({
-            id: "zoom",
-            placement: "bottom-right",
-            style: {
-              margin: "21px",
-              // position: "absolute",
-              // right: "-20px",
-              // bottom: "-20px",
-              zIndex: "100",
-            },
-          }),
-        ]}
-        style={{
-          // zIndex: "-1",
-          position: "relative",
-        }}
+        onMove={(evt) => handleMapViewChange({ viewState: evt.viewState })}
+        ref={mapRef}
+        style={{ position: "relative" }}
       >
-        <Map
-          mapStyle="mapbox://styles/mapbox/streets-v12"
-          mapboxAccessToken={token}
-          id="pidayMap"
-          ref={mapRef}
-          // style={{
-          //   position: "absolute",
-          //   top: "0",
-          //   left: "0",
-          //   right: "0",
-          //   bottom: "-50px",
-          // }}
-        >
-          {/* <div style={{ position: "absolute", right: 10, top: 10 }}>
-            <GeolocateControl
-              position="bottom-left"
-              style={{ zIndex: 110, position: "relative" }}
-              trackUserLocation
-            />
-          </div>
-          <NavigationControl position="bottom-right" /> */}
-        </Map>
-      </DeckGL>
-      <div className="absolute top-4 m-4">
-        <Card className=" w-max z-[1000]" size="sm">
-          <div
-            className="flex gap-2"
-            onClick={() => {
-              // console.log(viewState);
-            }}
-          >
+        <div style={{ position: "absolute", right: 10, top: 10 }}>
+          <GeolocateControl
+            position="bottom-left"
+            style={{ zIndex: 110, position: "relative" }}
+            trackUserLocation
+          />
+        </div>
+        <NavigationControl position="bottom-right" />
+
+        <GeocoderControl mapboxAccessToken={token} position="top-right" />
+      </Map>
+      <div className="absolute top-4 ml-4 mt-0">
+        <Card className="w-max z-[1000]" size="sm">
+          <div className="flex gap-2">
             <Typography level="body-sm">
               Latitude: {viewState.latitude.toFixed(4)}
             </Typography>
